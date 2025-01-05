fb88984f26c87c613bc811bcfc3ac0e355219fa2
vishalpatil1337 <30390447+vishalpatil1337@users.noreply.github.com>
2025-01-05 15:38:17 +0530

    Update FW-Review-Starter.py

--- FW-Review-Starter.py
+++ FW-Review-Starter.py
@@ -106,6 +106,8 @@ def main():
     # 22. Execute CDE-OOS-Checker
     if echo_message("Step 22: Executing CDE-OOS-Checker..."):
         execute_script("CDE-OOS-Checker.py")
+        execute_script("formating-findings.py")
+        execute_script("formating-firewall_analysis_results.py")
 
     # 23. Finding message
     echo_message("Step 23: Findings.xlsx file will be generated, check to find results...")
